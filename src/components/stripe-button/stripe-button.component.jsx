811361582209af866f2de69baa396ea3152af10b
tijani ademola <tijaniademola25@yahoo.com>
2020-01-27 15:45:48 +0100

    updating stripe integration and updating the favcon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({price})  => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_nDhDxLsJmGLx3bY6Q2JIr7ni00V2yQ2vbg'
+
+    const onToken = token => {
+            console.log(token);
+            alert('Payment Successful');
+        }
+    return (
+        <StripeCheckout
+        lable='Pay Now'
+        name= 'CRWN CLotthing ltd.'
+        bilingAddress
+        shippingAddress
+        image ='https://sendeyo.com/up/d/f3eb2117da'
+        description=  {`Your Total is $${price}`}
+        amount='{priceForStripe}'
+        panelLable='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+
+        />
+    );
+
+};
+
+export default StripeCheckoutButton;
